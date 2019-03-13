8276e9724e40488e77f1924e4cb235808817510e
Joe.Zhou266 <Joe.zhou266@gmail.com>
2019-03-13 09:04:59 -0400

    change java

---
+++ PerDiemsBatchOutStanding.java
@@ -0,0 +1,43 @@
+package com.ish.perDiemsBatch;
+
+import java.util.Date;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PerDiemsBatchOutStanding {
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("job/job-account-posting-jdbc.xml");
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+//		JobRepository jobRepository = (JobRepository) context.getBean("jobRepository");
+		Job job = (Job) context.getBean("job_accountPosting");		
+		JobParametersBuilder jobParametersBuilder = new JobParametersBuilder().addDate("date", new Date()).addString("id_begin", "1").addString(
+				"id_end", "999999999");		
+		try {			
+
+			JobExecution result = jobLauncher.run(job,
+					jobParametersBuilder.toJobParameters());
+
+			ExitStatus es = result.getExitStatus();
+			if (es.getExitCode().equals(ExitStatus.COMPLETED.getExitCode())) {
+				System.out.println("Job succeeded.");
+			} else {
+				System.out.println("Joe faild, exitCode=" + es.getExitCode());
+			}
+
+		}catch (Exception e) { 
+			
+			e.printStackTrace();
+		}
+    }
+
+}
+
